da5573197b5c4fecf537bd6e5b28dd8035c9e6ed
jiang.an <jarancn@gmail.com>
2018-12-11 16:14:53 +0800

    modify package name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="example_pkg",
+    name="wonderjar_pkg",
     version="0.0.1",
     author="wonderjar",
     author_email="jarancn@gmail.com",
